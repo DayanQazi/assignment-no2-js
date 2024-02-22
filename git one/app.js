7396fd3a805f3969b8051c3d7efad21bb0c782bd
Dayan Qazi <dayanqazi622@gmail.com>
2024-02-23 04:36:58 +0500

    work done

---
+++ git one/app.js
@@ -0,0 +1,29 @@
+// var y = 10;
+// var v = 5;
+// var w = 2;
+
+// var result = y - w * v + (w + y) + y;
+//             // 10 - 2 * 5 (2 + 10) + 10
+// console.log(result);
+
+
+
+// var num1 = 10;
+// var num2 = 5;
+
+// var res = num1++ + num1++ + ++num2 + ++num2 + ++num1
+
+// console.log(res)
+
+
+
+
+// var num1 = 10;
+// var num2 = 5;
+// var num3 = 7;
+
+// var res = num2++ - --num3 + ++num2 + num1-- + ++num3
+
+// console.log(res)
+
+
